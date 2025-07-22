9123626b47bf4575b6bbdf19460a954ff7f75813
Pedro Lima <pedro@yarilabs.com>
2025-07-22 15:19:12 +0100

    initial theme setup

---
+++ lua/griffin/groups.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+local colors = require("mycolorscheme.palette")
+
+M.setup = function()
+	return {
+
+		-- Standard
+		Comment = { fg = colors.comment, italic = true },
+
+		--Treesitter
+		["@string"] = { fg = colors.string },
+	}
+end
+
+return M
---
+++ lua/griffin/palette.lua
@@ -0,0 +1,41 @@
+return {
+	editor_background = "#1a191e",
+
+	background = "#17171b",
+	editor_foreground = "#eeffff",
+
+	ui_background = "#5F5E62",
+	ui_foreground = "#FAFFFF",
+
+	ui_accent_blue1 = "#609BB3",
+	ui_accent_blue2 = "#456F80",
+	ui_accent_blue3 = "#375866",
+
+	ui_accent_lightblue = "#89ddff",
+	ui_accent_darkblue = "#82aaff",
+
+	ui_blue = "#415580",
+	ui_blue2 = "#27334C",
+
+	ui_accent_yellow1 = "#FFCB6B",
+	ui_accent_yellow2 = "#E6B760",
+
+	ui_accent_green = "#c3e88d",
+
+	ui_accent_red = "#f07178",
+	ui_accent_red2 = "#9b2231",
+
+	ui_accent_orange = "#f89406",
+	ui_accent_orange2 = "#f78c6c",
+
+	ui_accent_purple = "#c792ea",
+
+	ui_accent_black = "#202020",
+
+	ui_info = "#3E7E00",
+	ui_line = "#395F65",
+	ui_shaded = "#546e7a",
+
+	ui_accent_info = "#57B000",
+	ui_accent_warning = "#CC4630",
+}
